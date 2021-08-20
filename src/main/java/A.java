4c7d84277b6950bd4ad4611712bcccbfe909982d
windy2015 <1922819391@qq.com>
2021-08-20 16:45:15 +0800

    master分支提交

--- src/main/java/A.java
+++ src/main/java/A.java
@@ -4,7 +4,7 @@
 public class A {
 
     public static void main(String[] args){
-        System.out.println("this is bugFix");
+        System.out.println("this is master------");
         System.out.println("bugFix1 commit!");
         System.out.println("master commit!");
     }
