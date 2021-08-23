db13ded2350d75f559d60df34b840238808cf04d
windy2015 <1922819391@qq.com>
2021-08-23 13:35:16 +0800

    commit2

---
+++ src/main/java/HelloGit.java
@@ -0,0 +1,12 @@
+/**
+ * Created by admin on 2021/8/23.
+ */
+public class HelloGit {
+
+    public static void main(String[] args){
+        System.out.println("commit1");
+        System.out.println("commit2");
+
+    }
+
+}
